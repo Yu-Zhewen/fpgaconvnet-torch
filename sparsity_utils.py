0f47be508278f4894e6102fe84f5cb35e628a7d4
zy18 <yuzhewen0108@163.com>
2022-08-11 09:07:08 +0100

    histogram at accum input

--- sparsity_utils.py
+++ sparsity_utils.py
@@ -39,7 +39,7 @@ def regsiter_hooks(model, coarse_in=-1):
             conv_input = conv_input.reshape((m.coarse_in, -1))
             num_of_zeros = m.coarse_in - torch.count_nonzero(conv_input, dim=0)
             coarse_in_sparsity = num_of_zeros / m.coarse_in
-            m.coarse_in_sparsity_hist += torch.histogram(coarse_in_sparsity.cpu(), bins=hist_bin_edges)[0]
+            m.sparsity_hist += torch.histogram(coarse_in_sparsity.cpu(), bins=hist_bin_edges)[0]
 
     handle_list = []
     for name, module in model.named_modules():
@@ -48,7 +48,7 @@ def regsiter_hooks(model, coarse_in=-1):
             if coarse_in == -1:
                 module.layer_sparsity = AverageMeter('Layer Sparsity', ':.4e')
             elif is_coarse_in_feasible(module, coarse_in):
-                module.coarse_in_sparsity_hist = torch.zeros(len(hist_bin_edges)-1)
+                module.sparsity_hist = torch.zeros(len(hist_bin_edges)-1)
             else:
                 continue
             #handle = module.register_forward_hook(log_conv_input_sparsity)
@@ -67,22 +67,34 @@ def output_sparsity_to_csv(model_name, model, accum_input=False):
                 with open(file_path, mode='a') as f:
                     csv_writer = csv.writer(f)
                     csv_header= ["Layer Name", "Layer Type"]
-                    if module.coarse_in == -1:
-                        if accum_input:
+                    if accum_input:
+                        if not module.kernel_distribution:
                             csv_header += ["Accum Input Sparsity (Zeros / im2col(NCHW))"]
                         else:
-                            csv_header += ["Layer Input Overall Sparsity (Zeros / NCHW)"]
+                            csv_header += ["KERNEL*KERNEL", "Accum Input Kernel Sparsity Histogram (Zeros / im2col(KK))"]
                     else:
-                        csv_header += ["COARSE_IN", "Coarse In Sparsity Histogram (Zeros / COARSE_IN)"]
+                        if module.coarse_in == -1:
+                            csv_header += ["Layer Input Overall Sparsity (Zeros / NCHW)"]
+                        else:
+                            csv_header += ["COARSE_IN", "Layer Input Coarse In Sparsity Histogram (Zeros / COARSE_IN)"]
+
+                            
                     csv_writer.writerow(csv_header)
 
             with open(file_path, mode='a') as f:
                 csv_writer = csv.writer(f)
                 new_row = [name, type(module)]
-                if module.coarse_in == -1:
-                    new_row += [module.layer_sparsity.avg.item()]
-                elif is_coarse_in_feasible(module, module.coarse_in):
-                    new_row += [module.coarse_in, module.coarse_in_sparsity_hist.type(torch.int64).tolist()]
+                if accum_input:
+                    if not module.kernel_distribution:
+                        new_row += [module.layer_sparsity.avg.item()]
+                    else:
+                        new_row += [module.kk, module.sparsity_hist.type(torch.int64).tolist()]
+                else:
+                    if module.coarse_in == -1:
+                        new_row += [module.layer_sparsity.avg.item()]
+                    else:
+                        new_row += [module.coarse_in, module.sparsity_hist.type(torch.int64).tolist()]
+                    
                 csv_writer.writerow(new_row)
 
 def delete_hooks(model, handle_list):
@@ -93,17 +105,21 @@ def delete_hooks(model, handle_list):
         if isinstance(module, nn.Conv2d) :#or isinstance(module, nn.Linear):
             if hasattr(module, "layer_sparsity"):
                 del module.layer_sparsity
-            if hasattr(module, "coarse_in_sparsity_hist"):
-                del module.coarse_in_sparsity_hist
+            if hasattr(module, "sparsity_hist"):
+                del module.sparsity_hist
 
 class VanillaConvolutionWrapper(nn.Module):
-    def __init__(self, conv_module):
+    def __init__(self, conv_module, kernel_distribution=False):
         super(VanillaConvolutionWrapper, self).__init__()
 
         self.conv_module = conv_module
+        self.conv_module.kernel_distribution = kernel_distribution
         self.run_reference = False
-
+        self.conv_module.kk = np.prod(self.conv_module.kernel_size)
+        self.hist_bin_edges = torch.linspace(0, self.conv_module.kk+1, self.conv_module.kk+2)
+        self.conv_module.sparsity_hist = torch.zeros(len(self.hist_bin_edges)-1)
     def forward(self, x):
+
         # https://discuss.pytorch.org/t/make-custom-conv2d-layer-efficient-wrt-speed-and-memory/70175
         assert self.conv_module.padding_mode == 'zeros'
         x_padded = F.pad(input=x, pad=self.conv_module._reversed_padding_repeated_twice, mode='constant', value=0)
@@ -127,12 +143,19 @@ class VanillaConvolutionWrapper(nn.Module):
         for hi, wi in np.ndindex(patches.shape[1:3]):
             patch = patches[:,hi,wi].clone()
             patch = patch * self.conv_module.weight
-            num_of_nonzeros += torch.count_nonzero(patch)
+            if not self.conv_module.kernel_distribution:    
+                num_of_nonzeros += torch.count_nonzero(patch)
+            else:
+                tmp = patch.reshape((-1, self.conv_module.kk))
+                num_of_zeros = self.conv_module.kk - torch.count_nonzero(tmp, dim=1)
+                self.conv_module.sparsity_hist += torch.histogram(num_of_zeros.float().cpu(), bins=self.hist_bin_edges)[0]
+                
             y[:,hi,wi] = patch.sum(-1).sum(-1).sum(-1)
-            
-        num_of_zeros = num_of_elements - num_of_nonzeros
-        layer_sparsity = num_of_zeros / num_of_elements
-        self.conv_module.layer_sparsity.update(layer_sparsity, batch_size)
+
+        if not self.conv_module.kernel_distribution:    
+            num_of_zeros = num_of_elements - num_of_nonzeros
+            layer_sparsity = num_of_zeros / num_of_elements
+            self.conv_module.layer_sparsity.update(layer_sparsity, batch_size)
 
         if self.conv_module.bias is not None:
             bias = self.conv_module.bias.expand(batch_size, h_windows, w_windows, out_channels)
