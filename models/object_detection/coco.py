c718651b4b03be9268c8c29f51a82bfc9f4734c5
Yu-Zhewen <yuzhewen0108@163.com>
2023-11-10 22:07:15 +0000

    yolov8 onnx preprocess

--- models/object_detection/coco.py
+++ models/object_detection/coco.py
@@ -1,6 +1,10 @@
+import onnx
 import os
 import torch
 import yaml
+
+import numpy as np
+import onnx_graphsurgeon as gs
 
 from models.base import TorchModelWrapper
 # note: do NOT move ultralytic import to the top, otherwise the edit in settings will not take effect
@@ -44,3 +48,54 @@ class UltralyticsModelWrapper(TorchModelWrapper):
     def onnx_exporter(self, onnx_path):
         path = self.yolo.export(format="onnx", simplify=True)
         os.rename(path, onnx_path)
+        self.remove_detection_head_v8(onnx_path)
+
+    def remove_detection_head_v8(self, onnx_path):
+        graph = onnx.load(onnx_path)
+        graph = gs.import_onnx(graph)
+
+        max_idx = 0
+        for idx, node in enumerate(graph.nodes):
+            if node.name == "/model.22/Reshape":
+                reshape_l_idx = idx
+            if node.name == "/model.22/Reshape_1":
+                reshape_m_idx = idx
+            if node.name == "/model.22/Reshape_2":
+                reshape_r_idx = idx
+
+        # remove extra operations
+        del graph.nodes[reshape_r_idx:-1]
+        del graph.nodes[reshape_m_idx:]
+        del graph.nodes[reshape_l_idx:]
+
+        # get output layers
+        concat_l = next(filter(lambda x: x.name == "/model.22/Concat", graph.nodes))
+        concat_m = next(filter(lambda x: x.name == "/model.22/Concat_1", graph.nodes))
+        concat_r = next(filter(lambda x: x.name == "/model.22/Concat_2", graph.nodes))
+
+        # get the resize layers
+        resize = next(filter(lambda x: x.name == "/model.10/Resize", graph.nodes))
+        resize.inputs[1] = gs.Constant("roi_0", np.array([0.0,0.0,0.0,0.0]))
+        resize = next(filter(lambda x: x.name == "/model.13/Resize", graph.nodes))
+        resize.inputs[1] = gs.Constant("roi_1", np.array([0.0,0.0,0.0,0.0]))
+
+        # create the output nodes
+        output_l = gs.Variable("/model.22/Concat_output_0",   shape=concat_l.outputs[0].shape, dtype="float16")
+        output_m = gs.Variable("/model.22/Concat_1_output_0", shape=concat_m.outputs[0].shape, dtype="float16")
+        output_r = gs.Variable("/model.22/Concat_2_output_0", shape=concat_r.outputs[0].shape, dtype="float16")
+
+        # connect the output nodes
+        concat_l.outputs = [ output_l ]
+        concat_m.outputs = [ output_m ]
+        concat_r.outputs = [ output_r ]
+
+        # update the graph outputs
+        graph.outputs = [ concat_l.outputs[0], concat_m.outputs[0], concat_r.outputs[0] ]
+
+        # cleanup graph
+        graph.cleanup()
+
+        # save the reduced network
+        graph = gs.export_onnx(graph)
+        graph.ir_version = 8 # need to downgrade the ir version
+        onnx.save(graph, onnx_path)
