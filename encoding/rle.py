11393cb601c380063e85ff05bf07c174c4803b2d
Yu-Zhewen <yuzhewen0108@163.com>
2023-12-17 14:45:58 +0000

    fix bug

--- encoding/rle.py
+++ encoding/rle.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 
 from models.classification.utils import AverageMeter
-from quantization.utils import ACTIVA_QUANT_MODULES, WEIGHT_QUANT_MODULES, \
+from quantization.utils import QuantMode, \
+    ACTIVA_QUANT_MODULES, WEIGHT_QUANT_MODULES, \
     QuantAct, linear_quantize, saturate
 
 ACTIVA_ENCODE_MODULES = ACTIVA_QUANT_MODULES
@@ -11,46 +12,45 @@ WEIGHT_ENCODE_MODULES = WEIGHT_QUANT_MODULES
 def get_compression_ratio(x, encoded_x, x_bits, l_bits):
     return (len(encoded_x) * (x_bits + l_bits)) / (len(x.flatten()) * x_bits)
 
-def encode(x, word_length, scaling_factor, zero_point, l_bits, max_process_length=2 ** 32):
+def encode(x, word_length, scaling_factor, zero_point, l_bits, transpose=False):
     # convert to quantized int representation
+    if transpose:
+        x = x.transpose(0, 1)
     x_quant = linear_quantize(x, scaling_factor, zero_point)
     x_quant = saturate(x_quant, word_length)
+    if transpose:
+        x_quant = x_quant.transpose(0, 1)
 
-    # todo: fix the dim order as hw streaming
-    x_flatten_total = x_quant.flatten()
-    combined_total = torch.tensor([], device=x.device)
+    # todo: fix the flatten dim order to match hw streaming
+    x_flatten = x_quant.flatten()
 
-    # split into chunks to avoid memory overflow
-    for i in range(0, len(x_flatten_total), max_process_length):
-        x_flatten = x_flatten_total[i:min(i + max_process_length, len(x_flatten_total))]
+    diff = torch.cat((torch.tensor([1], device=x.device), torch.diff(x_flatten)))
+    indices = torch.nonzero(diff != 0).flatten()
+    lengths = torch.diff(torch.cat((indices, torch.tensor([len(x_flatten)], device=x.device))))
 
-        diff = torch.cat((torch.tensor([1], device=x.device), torch.diff(x_flatten)))
-        indices = torch.nonzero(diff != 0).flatten()
-        lengths = torch.diff(torch.cat((indices, torch.tensor([len(x_flatten)], device=x.device))))
+    # combine consecutive elements and counts
+    combined = torch.stack((x_flatten[indices], lengths)).T
 
-        # combine consecutive elements and counts
-        combined = torch.stack((x_flatten[indices], lengths)).T
-
-        # split elements if the length exceeds 2^l_bits
-        mask = combined[:, 1] > 2 ** l_bits
-        split_indices = torch.nonzero(mask).flatten()
-        for index in reversed(split_indices):  # reverse order to avoid index shifts
-            value, length = combined[index]
-            repeat = int(length / (2 ** l_bits))
-            additional_elements = [[value, 2 ** l_bits]] * repeat
-            remain = length - repeat * (2 ** l_bits)
-            if remain > 0:
-                additional_elements.append([value, remain])
-            additional_elements = torch.tensor(additional_elements, device=x.device)
-            combined = torch.cat((combined[:index], additional_elements, combined[index + 1:]), dim=0)
-        combined_total = torch.cat((combined_total, combined), dim=0)
+    # split elements if the length exceeds 2^l_bits
+    mask = combined[:, 1] > 2 ** l_bits
+    split_indices = torch.nonzero(mask).flatten()
+    for index in reversed(split_indices):  # reverse order to avoid index shifts
+        value, length = combined[index]
+        repeat = int(length / (2 ** l_bits))
+        additional_elements = [[value, 2 ** l_bits]] * repeat
+        remain = length - repeat * (2 ** l_bits)
+        if remain > 0:
+            additional_elements.append([value, remain])
+        additional_elements = torch.tensor(additional_elements, device=x.device)
+        combined = torch.cat((combined[:index], additional_elements, combined[index + 1:]), dim=0)
         
-    return combined_total
+    return combined
 
 def log_encoding(module, input, output):
     batch_size = input[0].shape[0]
     encoded_data = encode(input[0], module.word_length, 
-        module.scaling_factor, module.zero_point, module.l_bits)
+        module.scaling_factor, module.zero_point, module.l_bits, 
+        (module.mode == QuantMode.CHANNEL_BFP))
     ratio = get_compression_ratio(input[0], encoded_data, module.word_length, module.l_bits)
     module.encoding_ratio.update(ratio, batch_size)
 
@@ -79,6 +79,8 @@ def encode_model(model_wrapper, l_bits):
     model_wrapper.inference("calibrate")
     for name, module in model_wrapper.named_modules():
         if isinstance(module, QuantAct):
+            if name[:-2] not in encode_info.keys():
+                encode_info[name[:-2]] = {}
             if name.endswith(".0"):
                 encode_info[name[:-2]]["input_compression_ratio"] = module.encoding_ratio.avg
             elif name.endswith(".2"):
